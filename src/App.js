3d92849f4c9ee13676e54d7dd09d746d73506a2a
Jon Wilson <jonwilson@ucanr.edu>
2022-01-07 16:14:52 -0800

    initial commit; re-implementation of my linktree page

--- src/App.js
+++ src/App.js
@@ -1,23 +1,41 @@
-import logo from './logo.svg';
 import './App.css';
+import 'bulma/css/bulma.css';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import {faGithub, faInstagram, faLinkedin} from "@fortawesome/free-brands-svg-icons";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div style={{
+      background:'#ebeef1',
+      position:'absolute',
+      top:'0',
+      bottom:'0',
+      left:0,
+      right: 0,
+      padding:'3em'
+    }}>
+      <div className="container has-text-centered">
+        <img src="/jw-512x512.png" style={{width:'128px', borderRadius:'50%'}} alt="Portrait of Jon Ash Wilson" />
+        <div className="mt-3 mb-5">
+          <div className="is-size-4">
+            Jon Ash Wilson
+          </div>
+          <div>
+            Software Developer and Artist
+          </div>
+        </div>
+        <div className="is-size-2 social-icons">
+          <a href="https://www.linkedin.com/in/jonashw/" className="icon has-text-black mx-4">
+            <FontAwesomeIcon icon={faLinkedin} />
+          </a>
+          <a href="https://github.com/jonashw" className="icon has-text-black mx-4">
+            <FontAwesomeIcon icon={faGithub} />
+          </a>
+          <a href="https://instagram.com/wilsonjonash" className="icon has-text-black mx-4">
+            <FontAwesomeIcon icon={faInstagram} />
+          </a>
+        </div>
+      </div>
     </div>
   );
 }
