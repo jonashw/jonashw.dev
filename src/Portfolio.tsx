608a77c5f58a7c71414a4a8ff1243cabc12b7915
Jon Wilson <2jonwilson@gmail.com>
2023-08-12 21:33:32 +0000

    set cache granularity to something less than *forever*.

--- src/Portfolio.tsx
+++ src/Portfolio.tsx
@@ -1,51 +1,77 @@
-import React from 'react';
-import './Portfolio.css';
-import PortfolioApplicationModal from './PortfolioApplicationModal';
-import { PortfolioSystem } from './PortfolioSystem';
+import React from "react";
+import "./Portfolio.css";
+import PortfolioApplicationModal from "./PortfolioApplicationModal";
+import { PortfolioSystem } from "./PortfolioSystem";
 
 function Portfolio() {
-  const [portfolioSystems,setPortfolioSystems] = React.useState<PortfolioSystem[]>([]);
-	const [activeSystem, setActiveSystem] = React.useState<PortfolioSystem | undefined>(undefined);
+  const [portfolioSystems, setPortfolioSystems] = React.useState<
+    PortfolioSystem[]
+  >([]);
+  const [activeSystem, setActiveSystem] = React.useState<
+    PortfolioSystem | undefined
+  >(undefined);
 
   React.useEffect(() => {
-    fetch('https://storage.googleapis.com/jonashw-dev-personal-website-public-data/portfolio-systems.json')
-    .then(r => r.json())
-    .then(records => {
-      setPortfolioSystems(records);
-    });
+    let d = new Date();
+    let now = [d.getFullYear(), d.getMonth(), d.getDay(), d.getHours()].join(
+      "-"
+    ); //set cache granularity to something less than *forever*.
+    fetch(
+      "https://storage.googleapis.com/jonashw-dev-personal-website-public-data/portfolio-systems.json?now=" +
+        now
+    )
+      .then((r) => r.json())
+      .then((records) => {
+        setPortfolioSystems(records);
+      });
   }, []);
 
   return (
-		<div className="container">
-			<div className="mb-5">
-				<h4>Software Application Portfolio</h4>
-				<p>I've had the privilege to work on the following software applications.</p>
-			</div>
+    <div className="container">
+      <div className="mb-5">
+        <h4>Software Application Portfolio</h4>
+        <p>
+          I've had the privilege to work on the following software applications.
+        </p>
+      </div>
 
-			<PortfolioApplicationModal system={activeSystem} setSystem={setActiveSystem} />
-			
-			<div className="row row-cols-1 row-cols-md-2 row-cols-lg-4 g-4">
-				{portfolioSystems.map((s, i) => (
-					<div key={i}>
-						<div className="card h-100" onClick={() => {
-							setActiveSystem(s !== activeSystem ? s : undefined);
-						}}>
-							{(s.Screenshots || [{ url: "https://bulma.io/images/placeholders/1280x960.png" }]).slice(0, 1).map((ss,i) => (
-								<img src={ss.url} alt={"Screenshot of " + s.Title} className="card-img-top" key={i}/>
-							))}
-							<div className="card-body">
-								<h5 className="card-title">
-									{s.Title}
-								</h5>
-								<h6 className="card-subtitle mb-2 text-muted">
-									{s.Subtitle}
-								</h6>
-							</div>
-						</div>
-					</div>
-				))}
-			</div>
-		</div>
+      <PortfolioApplicationModal
+        system={activeSystem}
+        setSystem={setActiveSystem}
+      />
+
+      <div className="row row-cols-1 row-cols-md-2 row-cols-lg-4 g-4">
+        {portfolioSystems.map((s, i) => (
+          <div key={i}>
+            <div
+              className="card h-100"
+              onClick={() => {
+                setActiveSystem(s !== activeSystem ? s : undefined);
+              }}
+            >
+              {(
+                s.Screenshots || [
+                  { url: "https://bulma.io/images/placeholders/1280x960.png" },
+                ]
+              )
+                .slice(0, 1)
+                .map((ss, i) => (
+                  <img
+                    src={ss.url}
+                    alt={"Screenshot of " + s.Title}
+                    className="card-img-top"
+                    key={i}
+                  />
+                ))}
+              <div className="card-body">
+                <h5 className="card-title">{s.Title}</h5>
+                <h6 className="card-subtitle mb-2 text-muted">{s.Subtitle}</h6>
+              </div>
+            </div>
+          </div>
+        ))}
+      </div>
+    </div>
   );
 }
 
